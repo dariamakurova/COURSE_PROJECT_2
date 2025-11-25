2fa70902638f8c97da9e37121f031187d4f40c1d
Daria_Makurova <dariamakurova@gmail.com>
2025-11-26 00:33:24 +0300

    classes simplified

--- src/head_hunter_api.py
+++ src/head_hunter_api.py
@@ -1,26 +1,27 @@
 from src.vacancies_api import VacanciesAPI
 import requests
+import json
 
 class HeadHunterAPI(VacanciesAPI):
     """ Класс для получения вакансий с платформы HeadHunter"""
 
-    def __init__(self, file_worker):
+    def __init__(self):
         self.url = 'https://api.hh.ru/vacancies'
         self.headers = {'User-Agent': 'HH-User-Agent'}
         self.params = {'text': '', 'page': 0, 'per_page': 100}
         self.vacancies = []
-        super().__init__(file_worker)
 
-    def load_vacancies(self, keyword):
+    def get_vacancies(self, keyword):
         self.params['text'] = keyword
         while self.params.get('page') != 20:
             response = requests.get(self.url, headers=self.headers, params=self.params)
             vacancies = response.json()['items']
             self.vacancies.extend(vacancies)
             self.params['page'] += 1
+        return json.dumps(self.vacancies, ensure_ascii=False, indent=4)
 
-    def get_vacancies(self):
-        return self.vacancies
 
-    def save(self):
-        self.file_worker.write(self.vacancies)
+if __name__ == "__main__":
+
+    vacancies = HeadHunterAPI()
+    print(vacancies.get_vacancies("Python, Москва"))
--- src/vacancies_api.py
+++ src/vacancies_api.py
@@ -3,20 +3,8 @@ from abc import ABC, abstractmethod
 class VacanciesAPI(ABC):
     """ Абстрактный класс для получения вакансий по API """
 
-    def __init__(self, file_worker):
-        self.file_worker = file_worker
 
     @abstractmethod
-    def load_vacancies(self, *args, **kwargs):
+    def get_vacancies(self, *args, **kwargs):
         """ Загрузка вакансий с платформы"""
-        pass
-
-    @abstractmethod
-    def get_vacancies(self):
-        """ Получение текущего списка вакансий """
-        pass
-
-    @abstractmethod
-    def save(self):
-        """ Сохранение вакансий в файл """
         pass
