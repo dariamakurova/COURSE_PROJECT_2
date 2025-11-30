a1200d4b6bbc6e2e5a03e08aa05d9752ef4fcabb
Daria_Makurova <dariamakurova@gmail.com>
2025-12-01 01:09:56 +0300

    saver abstract class added

--- src/saver.py
+++ src/saver.py
@@ -4,16 +4,23 @@ class Saver(ABC):
     """ Абстрактный класс для работы с файлами - добавление вакансий в файл, получение данных из файла
     по указанным критериям и удаление информации о вакансиях """
 
-    def __init__(self, filename=None):
-        if filename:
-            self.filename = filename
-        else:
-            self.filename = "vacancies"
 
     @abstractmethod
     def add_vacancy(self, *args, **kwargs):
+        """ Добавление вакансии в файл """
         pass
 
     @abstractmethod
     def delete_vacancy(self, *args, **kwargs):
+        """ Удаление вакансии """
+        pass
+    @abstractmethod
+    def get_vacancies(self, *args, **kwargs):
+        """ Получение вакансий по ключу """
+        pass
+
+    def connect(self):
+        pass
+
+    def close(self):
         pass
