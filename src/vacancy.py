2ad55c5a4ced9c926ce5ee0531ab9a9a59d980c0
Daria_Makurova <dariamakurova@gmail.com>
2025-12-01 00:44:39 +0300

    class vacancy with comparing methods

--- src/vacancy.py
+++ src/vacancy.py
@@ -5,25 +5,15 @@ class Vacancy():
     """ Класс для вакансий """
 
     def __init__(self, name, url, salary_from=None, salary_to=None, requirement=""):
-        self.name = self._validate_name(name)
-        self.url = self._validate_url(url)
-        self.salary_from = salary_from
-        self.salary_to = salary_to
-        self.requirement = requirement
-
-    @staticmethod
-    def _validate_name(name):
         if not name or not isinstance(name, str):
-            return "Без названия"
-        return name.strip()
+            self.name = "Без названия"
+        else:
+            self.name = name
 
-    @staticmethod
-    def _validate_url(url):
         if not url or not isinstance(url, str) or not url.startswith("http"):
-            return "Ссылка не указана"
-        return url.strip()
-
-    def _validate_salary(self, salary_from, salary_to):
+            self.url = "Ссылка не указана"
+        else:
+            self.url = url
 
         if isinstance(salary_from, dict) and salary_to is None:
             salary_to = salary_from.get('to')
@@ -35,4 +25,26 @@ class Vacancy():
         s_from = max(0, s_from) if s_from is not None else 0
         s_to = max(0, s_to) if s_to is not None else 0
 
-        return s_from, s_to
+        self.salary_from = s_from
+        self.salary_to = s_to
+
+        self.salary = max(self.salary_from, self.salary_to)
+
+        self.requirement = requirement or ""
+
+
+    def __lt__(self, other):
+        """ Метод для операции сравнения «меньше» """
+        return self.salary < other.salay
+
+    def __le__(self, other):
+        """ Метод для операции сравнения «меньше или равно» """
+        return self.salary <= other.salary
+
+    def __gt__(self, other):
+        """ метод для операции сравнения «больше» """
+        return self.salary > other.salay
+
+    def __ge__(self, other):
+        """ Метод для операции сравнения «больше или равно» """
+        return self.salary >= other.salary
